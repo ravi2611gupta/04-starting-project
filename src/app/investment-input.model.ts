166e03e40c0622d3d00eb7a48c952aab88cbe3c3
ravi <rvi2611gupta>
2024-12-18 18:56:52 +0530

    81 videos complated

---
+++ src/app/investment-input.model.ts
@@ -0,0 +1,13 @@
+export interface InvestmentInput {
+  initialInvestment: number;
+  annualInvestment: number;
+  duration: number;
+  expectedReturn: number;
+}
+
+// export type InvestmentInput = {
+//   initialInvestment: number;
+//   annualInvestment: number;
+//   duration: number;
+//   expectedReturn: number;
+// };
